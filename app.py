43ca209ce62e55f8e8fdbc5402865904bc47730f
Muhammad Shahid <shaikh.mshahid2010@gmail.com>
2026-07-14 16:49:13 +0500

    Updated home route

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello World!"
+@app.route("/")
+def home():
+    return "Welcome to Student Enrollment System"
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
