5a53ecce75968a8b704af76924da0f11af36ee0b
Muhammad Shahid <shaikh.mshahid2010@gmail.com>
2026-07-14 16:57:01 +0500

    Updated home route

--- app.py
+++ app.py
@@ -1,10 +1,41 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
+students = [
+    {
+        "id": 1,
+        "name": "Arsalan Khan",
+        "course": "Artificial Intelligence",
+        "email": "arsalan@example.com"
+    },
+    {
+        "id": 2,
+        "name": "Zainab Ahmed",
+        "course": "Web Development",
+        "email": "zainab@example.com"
+    },
+    {
+        "id": 3,
+        "name": "Bilal Siddiqui",
+        "course": "Cyber Security",
+        "email": "bilal@example.com"
+    },
+    {
+        "id": 4,
+        "name": "Ayesha Umar",
+        "course": "Data Science",
+        "email": "ayesha@example.com"
+    }
+]
+
+@app.route('/')
 def home():
     return "Welcome to Student Enrollment System"
 
-if __name__ == "__main__":
+@app.route('/test-data')
+def get_all_students():
+    return jsonify(students)
+
+if __name__ == '__main__':
     app.run(debug=True)
