ccabf370059633a55ee5ae5e255aed0fa796da8d
Muhammad Shahid <shaikh.mshahid2010@gmail.com>
2026-07-15 16:06:28 +0500

    Add Student via Form Data

--- app.py
+++ app.py
@@ -9,17 +9,31 @@ students = [
     {"id": 4, "name": "Ayesha Umar", "course": "Data sci", "email": "ayesha@example.com"}
 ]
 
-@app.route('/students', methods=['GET'])
-def get_students():
-    course_filter = request.args.get('course')
+@app.route('/add-student-form', methods=['POST'])
+def add_student_form():
+    name = request.form.get('name')
+    course = request.form.get('course')
+    email = request.form.get('email')
     
-    if course_filter:
-        filtered_students = [
-     s for s in students 
-    if s["course"].strip().lower() == course_filter.strip().lower()
-        ]
-    return jsonify(filtered_students), 200
+    if not name or not course or not email:
+        return jsonify({"error": "Missing required fields: name, course, and email are all required!"}), 400
         
-    return jsonify(students), 200
+    new_id = max([s["id"] for s in students]) + 1 if students else 1
+    
+    new_student = {
+        "id": new_id,
+        "name": name,
+        "course": course,
+        "email": email
+    }
+    
+    students.append(new_student)
+    
+    return jsonify({
+        "message": "Student added successfully via Form Data!",
+        "student": new_student
+    }), 201
+
+
 if __name__ == '__main__':
     app.run(debug=True)
