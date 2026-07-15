b794cf96d117404a46cc3b756be8fefb30291ca9
Muhammad Shahid <shaikh.mshahid2010@gmail.com>
2026-07-15 16:48:47 +0500

    Add Student via JSON

--- app.py
+++ app.py
@@ -3,17 +3,22 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 students = [
-    {"id": 1, "name": "Arsalan Khan", "course": "AI", "email": "arsalan@example.com"},
-    {"id": 2, "name": "Zainab Ahmed", "course": "Web Dev", "email": "zainab@example.com"},
-    {"id": 3, "name": "Bilal Siddiqui", "course": "Cyber Sec", "email": "bilal@example.com"},
-    {"id": 4, "name": "Ayesha Umar", "course": "Data sci", "email": "ayesha@example.com"}
+    {"id": 1, "name": "Arsalan Khan", "course": "Artificial Intelligence", "email": "arsalan@example.com"},
+    {"id": 2, "name": "Zainab Ahmed", "course": "Web Development", "email": "zainab@example.com"},
+    {"id": 3, "name": "Bilal Siddiqui", "course": "Cyber Security", "email": "bilal@example.com"},
+    {"id": 4, "name": "Ayesha Umar", "course": "Artificial Intelligence", "email": "ayesha@example.com"}
 ]
 
-@app.route('/add-student-form', methods=['POST'])
-def add_student_form():
-    name = request.form.get('name')
-    course = request.form.get('course')
-    email = request.form.get('email')
+@app.route('/add-student-json', methods=['POST'])
+def add_student_json():
+    data = request.get_json()
+    
+    if not data:
+        return jsonify({"error": "Invalid request, JSON body is required!"}), 400
+        
+    name = data.get('name')
+    course = data.get('course')
+    email = data.get('email')
     
     if not name or not course or not email:
         return jsonify({"error": "Missing required fields: name, course, and email are all required!"}), 400
@@ -30,10 +35,8 @@ def add_student_form():
     students.append(new_student)
     
     return jsonify({
-        "message": "Student added successfully via Form Data!",
+        "message": "Student added successfully via JSON!",
         "student": new_student
     }), 201
-
-
 if __name__ == '__main__':
     app.run(debug=True)
